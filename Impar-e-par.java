8e30ce4660e12ebfcdb5146aefcb009bb3ee73a2
Rafael William Dresch <117855490+RafinhaWD@users.noreply.github.com>
2024-03-29 19:14:21 -0300

    Update Impar-e-par.java

--- Impar-e-par.java
+++ Impar-e-par.java
@@ -1,1 +1,42 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Collections;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        Integer[] numeros = {5, 2, 8, 1, 9, 4};
+        List<Integer> numerosList = Arrays.asList(numeros);
+
+        List<Integer> pares = new ArrayList<>();
+        List<Integer> impares = new ArrayList<>();
+
+        for (int numero : numerosList) {
+            if (numero % 2 == 0) {
+                pares.add(numero);
+            } else {
+                impares.add(numero);
+            }
+        }
+
+
+        System.out.println("sem ordenacao");
+         System.out.println(pares);
+        System.out.println(impares);
+
+        Collections.sort(pares);
+        Collections.sort(impares);
+
+        System.out.println("com ordenacao");
+        System.out.println(pares);
+        System.out.println(impares);
+
+        List<Integer> listaOrdenada = new ArrayList<>();
+        listaOrdenada.addAll(pares);
+        listaOrdenada.addAll(impares);
+
+        System.out.println("Lista ordenada" + listaOrdenada);
+}
+}
 
